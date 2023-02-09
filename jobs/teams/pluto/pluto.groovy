1c09b0d249ccbfc9f965012635d01f7befb735d0
opeomotayo <43703454+opeomotayo@users.noreply.github.com>
2023-02-09 06:25:23 +0000

    testing path

---
+++ jobs/teams/pluto/pluto.groovy
@@ -0,0 +1,26 @@
+// github('opeomotayo/cd-projects') can also be used instead of url('https://github.com/opeomotayo/cd-projects.git')
+// multiple pipelinesJobs can also be set here
+pipelineJob('actions-controller') {
+    logRotator {
+        numToKeep(10)
+        daysToKeep(30)
+    }
+    definition {
+        cps {
+            script(readFileFromWorkspace('pipeplines/actions-controller/Jenkinsfile'))
+            sandbox()     
+        }
+        // cpsScm {
+        //     scm {
+        //         git {
+        //         remote {
+        //             url('https://github.com/opeomotayo/cd-projects.git') 
+        //         }
+        //         branches('main')
+        //         }
+        //     }   
+        //     scriptPath('pipeplines/actions-controller/Jenkinsfile')
+        // }  
+    }       
+}
+
