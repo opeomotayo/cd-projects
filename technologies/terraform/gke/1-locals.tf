12e1bab741b12ce141003f311b1d2da841848453
opeomotayo <43703454+opeomotayo@users.noreply.github.com>
2023-03-01 09:13:42 +0000

    terraform gke

---
+++ technologies/terraform/gke/1-locals.tf
@@ -0,0 +1,14 @@
+locals {
+  region               = "eu-west2"
+  org_id               = "terraform-gke-379306"
+  billing_account      = "01F7F7-EEDB76-A58E66"
+  host_project_name    = "host-staging"
+  service_project_name = "k8s-staging"
+  host_project_id      = "${local.host_project_name}-${random_integer.int.result}"
+  service_project_id   = "${local.service_project_name}-${random_integer.int.result}"
+  projects_api         = "container.googleapis.com"
+  secondary_ip_ranges = {
+    "pod-ip-range"      = "10.0.0.0/14",
+    "services-ip-range" = "10.4.0.0/19"
+  }
+}
--- technologies/terraform/gke/main.tf
+++
@@ -1,5 +0,0 @@
-provider "google" {
-  credentials = file("~/encoded-secrets/terraform-sa.json")
-  project     = "terraform-provisioner"
-  region      = "eu-west-2"
-}
